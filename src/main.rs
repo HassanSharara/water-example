ee7c0111910b547af542c69f86286f7e3f1cd02a
pc <ft47fc@gmail.com>
2025-01-10 22:28:24 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+use water_http::server:: ServerConfigurations;
+use water_http::{InitControllersRoot, WaterController};
+
+type MainHolderType = HashMap<String,String>;
+InitControllersRoot!{
+   name:MAIN_ROOT,
+   holder_type:MainHolderType,
+}
+
+
+#[tokio::main]
+async fn main() {
+
+    let  configs = ServerConfigurations::bind("0.0.0.0",80);
+
+    water_http::RunServer!(
+       configs,
+       MAIN_ROOT,
+       MainController
+   );
+}
+WaterController! {
+   holder -> crate::MainHolderType,
+   name -> MainController,
+   functions -> {
+       GET => / => any_thing(context) async {
+           _=context.send_str("Hello there").await;
+       }
+   }
+
+}
